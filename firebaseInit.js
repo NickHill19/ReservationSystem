ca46b6f0a46d4b2049ce507efa1b08bd21311a67
Michael Buchman <mbookie213@gmail.com>
2018-10-30 23:00:43 -0500

    Add files via upload

---
+++ firebaseInit.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCpv6iBq0iM84EtpyjqBNwSCGLfL4AOfIs",
+    authDomain: "reservationsystem-3dd85.firebaseapp.com",
+    databaseURL: "https://reservationsystem-3dd85.firebaseio.com",
+    projectId: "reservationsystem-3dd85",
+    storageBucket: "reservationsystem-3dd85.appspot.com",
+    messagingSenderId: "385982147641"
+};
+firebase.initializeApp(config);
